f4320a99268e2c04c2f2c5da2d64e4ba2e479c93
danmiee <dan-mi@kakao.com>
2022-12-29 15:43:22 +0900

    Tsp 클래스 실행파일

---
+++ AIP_09_SearchAlgorithms/steepest_ascent_tsp_module.py
@@ -0,0 +1,49 @@
+from problem import Tsp
+
+
+def main():
+    # Tsp class instance 생성
+    cls_num = Tsp()
+    # setVariables를 이용해 문제 읽어오기
+    cls_num.setVariables()
+    # steepestAscent 실행
+    steepestAscent(cls_num)
+    # describe, report 이용해 결과 출력
+    cls_num.describe()
+    cls_num.report()
+
+
+def steepestAscent(cls_num):
+    current = cls_num.randomInit()
+    valueC = cls_num.evaluate(current)
+    while True:
+        neighbors = cls_num.mutants(current)
+        best, bestValue = bestOf(neighbors, cls_num)
+        if bestValue < valueC:
+            current = best
+            valueC = bestValue
+        else:
+            break
+    cls_num.storeResult(current, valueC)
+
+
+def bestOf(neighbors, cls_num):
+    best = neighbors[0]
+    bestValue = cls_num.evaluate(best)
+
+    for i in range(1, len(neighbors)):
+        newValue = cls_num.evaluate(neighbors[i])
+        if newValue < bestValue:
+            best = neighbors[i]
+            bestValue = newValue
+    return best, bestValue
+
+
+def displaySetting(cls_num):
+    print()
+    print("Search algorithm: Steepest-Ascent Hill Climbing")
+    print()
+    print("Mutation step size:", cls_num.getDelta())
+
+
+main()
